f69d8751968d926d641a8ce737f6f605e208f568
александр <85447129+Alexander-Guzhvin@users.noreply.github.com>
2023-05-29 16:28:45 +0300

    new

--- js/script.js
+++ js/script.js
@@ -224,7 +224,22 @@ var accordion = (function (element) {
 
 
 
- 
+  function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+  
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
 
 
 
